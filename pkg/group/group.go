a148a9de67a48d60fe0d94c758f21d1a720ebbab
ubuntu <i@ihypo.net>
2022-06-03 14:07:55 +0000

    update: init group (f*k ipad web idea)

--- pkg/group/group.go
+++ pkg/group/group.go
@@ -1,10 +1,33 @@
 package group
 
 import (
+	"context"
+	"github.com/basenana/nanafs/config"
+	"github.com/basenana/nanafs/pkg/storage"
 	"github.com/basenana/nanafs/pkg/types"
+	"go.uber.org/zap"
 )
 
-type Group struct {
-	types.Object
-	Rule Rule
+type Manager struct {
+	meta      storage.MetaStore
+	storage   storage.Storage
+	cfg       config.Config
+	cfgLoader config.Loader
+	logger    *zap.SugaredLogger
+}
+
+func (m *Manager) ListObjectChildren(ctx context.Context, obj *types.Object) ([]*types.Object, error) {
+	return nil, nil
+}
+
+func (m *Manager) listGroupChildren(ctx context.Context, obj *types.Object) ([]*types.Object, error) {
+	return nil, nil
+}
+
+func (m *Manager) listGroupChildren(ctx context.Context, obj *types.Object) ([]*types.Object, error) {
+	return nil, nil
+}
+
+func NewManager() *Manager {
+	return &Manager{}
 }
