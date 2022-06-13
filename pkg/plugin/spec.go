ffcc5700d7b233ac252e6404073e15dbb06b41df
Hypo <i@ihypo.net>
2022-06-13 23:34:08 +0800

    update: plugin spec

--- pkg/plugin/spec.go
+++ pkg/plugin/spec.go
@@ -1,4 +1,14 @@
 package plugin
 
-type name struct {
+const (
+	ExecType   = "goplugin"
+	BinType    = "bin"
+	ScriptType = "script"
+)
+
+// Spec is Plugin Config File to load a Plugin
+type Spec struct {
+	Type       string            `json:"type"`
+	Path       string            `json:"path"`
+	Parameters map[string]string `json:"parameters"`
 }
