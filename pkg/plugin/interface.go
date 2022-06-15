5f1f9cfd73f9216e64aa5cdfeeff2871ccdf811d
Hypo <i@ihypo.net>
2022-06-15 23:55:55 +0800

    update: plugin interface

--- pkg/plugin/interface.go
+++ pkg/plugin/interface.go
@@ -2,11 +2,13 @@ package plugin
 
 import (
 	"context"
+	"github.com/basenana/nanafs/pkg/files"
 	"github.com/basenana/nanafs/pkg/types"
 )
 
 type Plugin interface {
 	Name() string
+	Type() Type
 }
 
 type ProcessPlugin interface {
@@ -20,12 +22,12 @@ type MetaPlugin interface {
 
 type SourcePlugin interface {
 	Plugin
-	Run(ctx context.Context) error
+	Run(ctx context.Context) (<-chan *types.Object, error)
 }
 
 type MirrorPlugin interface {
 	Plugin
-	LookUp(ctx context.Context, path string) error
-	List(ctx context.Context, path string) error
-	Open(ctx context.Context, path string) error
+	LookUp(ctx context.Context, path string) (*types.Object, error)
+	List(ctx context.Context, path string) ([]*types.Object, error)
+	Open(ctx context.Context, path string, attr files.Attr) (files.File, error)
 }
